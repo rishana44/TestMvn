4d5b733a799e0bc079284f41d118877bf2a5855e
IrinaMakarenko <rishana44@gmail.com>
2023-12-22 13:24:59 +0300

    Homework for Lesson_3 (Part 2: Arrays)

---
+++ src/main/java/Lesson_3/Homework_3.java
@@ -0,0 +1,93 @@
+package Lesson_3;
+public class Homework_3 {
+    public static void main(String[] args) {
+
+        int[] replaceValues = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        int[] multiplyOn2 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        System.out.println("Task 6: ");
+        task6(replaceValues);
+        System.out.println("\nTask 7: ");
+        task7(100);
+        System.out.println("\nTask 8: ");
+        task8(multiplyOn2);
+        System.out.println("\nTask 9: ");
+        task9();
+        System.out.println("\nTask 10: ");
+        task10(2, 3);
+    }
+
+    /** Tasks from 6 to 10 (Arrays)
+     * 6. Задать целочисленный массив, состоящий из элементов 0 и 1.
+     * Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+     * С помощью цикла и условия заменить 0 на 1, 1 на 0;
+     */
+
+    public static void task6(int[] replaceValues) {
+        for (int i = 0; i < replaceValues.length; i++) {
+            replaceValues[i] = (replaceValues[i] > 0)? 0 : 1;
+            System.out.print(replaceValues[i] + " ");
+        }
+    }
+
+    /** Task 7
+     *7. Задать пустой целочисленный массив длиной 100.
+     * С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
+     */
+    public static void task7(int size) {
+        int[] fillArr = new int[size];
+        for (int i = 0; i < fillArr.length; i++) {
+            fillArr[i] = i + 1;
+            System.out.print(fillArr[i] + " ");
+        }
+    }
+
+    /** Task 8
+     * 8. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ]
+     * пройти по нему циклом, и числа меньшие 6 умножить на 2;
+     */
+    public static void task8(int[] multiplyOn2) {
+        for (int i = 0; i < multiplyOn2.length; i++) {
+            if (multiplyOn2[i] < 6) {
+                multiplyOn2[i] = multiplyOn2[i] * 2;
+            }
+            System.out.print(multiplyOn2[i] + " ");
+        }
+    }
+
+    /** Task 9
+     * 9. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+     *  и с помощью цикла(-ов) заполнить его диагональные элементы единицами
+     * (можно только одну из диагоналей, если обе сложно).
+     * Определить элементы одной из диагоналей можно по следующему принципу:
+     *  индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
+     */
+    public static void task9() {
+        int[][] arr = new int[3][3];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0, x = arr[i].length - 1; j < arr[i].length; j++, x--) {
+                if (i == j || i == x) arr[i][j] = 1;
+                else arr[i][j] = 0;
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    /** Task 10
+     * 10. Написать метод, принимающий на вход два аргумента: len и initialValue,
+     *  и возвращающий одномерный массив типа int длиной len, каждая ячейка которого
+     *  равна initialValue;
+     */
+
+    public static void task10
+    (int len, int initialValue){
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+            System.out.print("[" + i + "]" + arr[i] + " ");
+        }
+    }
+
+}
+
